bcbde7547ce67ca10be1df98c637e366aa4361ff
Steven Berry <stevenb@dnstech.com.au>
2016-08-18 13:38:07 +1000

    Add TestResult.cs hidden by .gitignore

---
+++ MsTestRunner/TestResult.cs
@@ -0,0 +1,46 @@
+﻿namespace MsTestRunner
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public sealed class TestResult
+    {
+        private static readonly HashAlgorithm Provider = new SHA1CryptoServiceProvider();
+            
+        public TestResult(string name, string output, bool success, TimeSpan duration)
+        {
+            this.Name = name;
+            this.Output = output;
+            this.Success = success;
+            this.Id = GuidFromString(name);
+            this.Duration = duration;
+        }
+
+        public readonly Guid Id;
+
+        public readonly Guid ExecutionId = Guid.NewGuid();
+
+        public readonly string Name;
+
+        public readonly string Output;
+
+        public readonly bool Success;
+
+        public readonly TimeSpan Duration;
+
+        /// Consistent Id based on string as done by MsTest
+        private static Guid GuidFromString(string data)
+        {
+            byte[] hash;
+            lock (Provider)
+            {
+                hash = Provider.ComputeHash(System.Text.Encoding.Unicode.GetBytes(data));
+            }
+
+            byte[] toGuid = new byte[16];
+            Array.Copy(hash, toGuid, 16);
+
+            return new Guid(toGuid);
+        }
+    }
+}
