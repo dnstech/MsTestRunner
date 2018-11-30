2deaf9161ee9c7918f1d0cbe8c46dec3e2b0bd77
Shailen Sukul <shailen.sukul@dnstech.com.au>
2018-11-30 16:37:16 +1100

    Added a Test Runner for .Net Core

---
+++ MsTestRunnerCore/Program.cs
@@ -0,0 +1,167 @@
+﻿// --------------------------------------------------------------------------------------------------
+//  <copyright file="Program.cs" company="DNS Technology Pty Ltd.">
+//    Copyright (c) 2015 DNS Technology Pty Ltd. All rights reserved.
+//  </copyright>
+// --------------------------------------------------------------------------------------------------
+
+namespace MsTestRunner
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        #region Methods
+
+        private static int Main(string[] args)
+        {
+            var testRunner = new TestRunner(Path.Combine(Environment.CurrentDirectory, "TestResults", DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss")));
+            var filter = false;
+            var parallelism = false;
+            string trxPath = null;
+
+            var testAssemblies = new List<string>();
+
+            foreach (var arg in args)
+            {
+                if (filter)
+                {
+                    filter = false;
+                    testRunner.AddFilter(arg);
+                }
+                else if (parallelism)
+                {
+                    parallelism = false;
+                    testRunner.Parallelism = int.Parse(arg);
+                }
+                else if (arg.Equals("-q", StringComparison.OrdinalIgnoreCase))
+                {
+                    testRunner.QuietMode = true;
+                }
+                else if (arg.Equals("-f", StringComparison.OrdinalIgnoreCase))
+                {
+                    filter = true;
+                }
+                else if (arg.Equals("-p", StringComparison.OrdinalIgnoreCase))
+                {
+                    parallelism = true;
+                }
+                else if (arg.StartsWith("/resultsfile:", StringComparison.OrdinalIgnoreCase))
+                {
+                    trxPath = arg.Substring("/resultsfile:".Length);
+                }
+                else if (!arg.StartsWith("-"))
+                {
+                    var path = arg.StartsWith("/testcontainer:") ? arg.Substring("/testcontainer:".Length) : arg;
+
+                    if (File.Exists(Path.GetFullPath(path)))
+                    {
+                        testAssemblies.Add(path);
+                    }
+                    else if (Directory.Exists(Path.GetFullPath(path)))
+                    {
+                        foreach (var filePath in Directory.EnumerateFiles(path, "*Tests.dll", SearchOption.AllDirectories))
+                        {
+                            testAssemblies.Add(filePath);
+                        }
+                    }
+                }
+            }
+
+            foreach (var assemblyFile in testAssemblies)
+            {
+                testRunner.AddTestAssembly(assemblyFile);
+            }
+
+            var result = testRunner.Execute();
+
+            if (trxPath != null)
+            {
+                TrxGenerator.Generate(trxPath, result);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine("Took {0} to Run {1} Tests Completed at {2}", result.TimeTaken, result.Succeeded + result.Failed, DateTime.Now.ToString("s"));
+            if (result.Succeeded > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} Succeeded", result.Succeeded);
+            }
+
+            if (result.Failed > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} Failed", result.Failed);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var interactive = args.Contains("-i", StringComparer.OrdinalIgnoreCase);
+            if (interactive)
+            {
+                InteractiveMode(result);
+            }
+            else
+            {
+                ListFailures(result);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadKey();
+            }
+
+            return result.Failed < int.MaxValue ? (int)result.Failed : int.MaxValue;
+        }
+
+        private static void ListFailures(TestRunResult result)
+        {
+            for (int i = 0; i < result.FailureMessages.Count; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Failure #{0} of {1}", i + 1, result.FailureMessages.Count);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(result.FailureMessages[i]);
+                Console.WriteLine();
+            }
+        }
+
+        private static void InteractiveMode(TestRunResult result)
+        {
+            var currentFailure = 0;
+            var lastKey = Console.ReadKey().Key;
+            while (lastKey != ConsoleKey.Escape && currentFailure < result.FailureMessages.Count)
+            {
+                Console.WriteLine("Failure #{0} of {1}", currentFailure + 1, result.FailureMessages.Count);
+                Console.WriteLine(result.FailureMessages[currentFailure]);
+                Console.WriteLine();
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow && currentFailure > 0)
+                {
+                    Console.Clear();
+                    currentFailure--;
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        Console.Clear();
+                        currentFailure++;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
---
+++ MsTestRunnerCore/TrxGenerator.cs
@@ -0,0 +1,119 @@
+﻿// --------------------------------------------------------------------------------------------------
+//  <copyright file="TrxGenerator.cs" company="DNS Technology Pty Ltd.">
+//    Copyright (c) 2016 DNS Technology Pty Ltd. All rights reserved.
+//  </copyright>
+// --------------------------------------------------------------------------------------------------
+namespace MsTestRunner
+{
+    using System;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    public static class TrxGenerator
+    {
+        public static void Generate(string path, TestRunResult result)
+        {
+            var outcome = result.Failed == 0 ? (result.Ignored == 0 ? "Completed" : "Warning") : "Failed";
+
+            var ns = XNamespace.Get("http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+
+            var testListId = Guid.NewGuid();
+            
+            var doc = new XDocument(new XElement(ns + "TestRun", 
+                new XAttribute("id", Guid.NewGuid()),
+                new XElement(ns + "TestSettings"),
+                new XElement(ns + "Times"),
+                new XElement(ns + "ResultSummary",
+                    new XAttribute("outcome", outcome),
+                    new XElement(ns + "Counters",
+                        new XAttribute("total", result.Succeeded + result.Failed + result.Ignored),
+                        new XAttribute("executed", result.Succeeded + result.Failed),
+                        new XAttribute("error", 0),
+                        new XAttribute("failed", result.Failed),
+                        new XAttribute("timeout", 0),
+                        new XAttribute("aborted", 0),
+                        new XAttribute("inconclusive", 0),
+                        new XAttribute("passedButRunAborted", 0),
+                        new XAttribute("notRunnable", 0),
+                        new XAttribute("notExecuted", 0),
+                        new XAttribute("disconnected", 0),
+                        new XAttribute("warning", 0),
+                        new XAttribute("completed", 0),
+                        new XAttribute("inProgress", 0),
+                        new XAttribute("pending", 0)
+                    ),
+                    new XElement(ns + "RunInfos")),
+                new XElement(ns + "TestDefinitions",
+                    result.Tests.Select(TestDefinition)),
+                new XElement(ns + "TestLists",
+                    new XElement(ns + "TestList",
+                        new XAttribute("name", "List"),
+                        new XAttribute("id", testListId))),
+                new XElement(ns + "TestEntries",
+                    result.Tests.Select(test => TestEntry(test, testListId))),
+                new XElement(ns + "Results",
+                    result.Tests.Select(test => TestResult(test, testListId)))
+                ));
+
+            doc.Save(path);
+        }
+
+        private static XElement TestDefinition(TestResult test)
+        {
+            var ns = XNamespace.Get("http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+
+            return new XElement(ns + "UnitTest",
+                new XAttribute("name", test.Name),
+                new XAttribute("storage", string.Empty),
+                new XAttribute("id", test.Id),
+                new XElement(ns + "Execution",
+                    new XAttribute("id", test.ExecutionId)),
+                new XElement(ns + "TestMethod",
+                     new XAttribute("codeBase", string.Empty),
+                     new XAttribute("adapterTypeName", string.Empty),
+                     new XAttribute("className", string.Empty),
+                     new XAttribute("name", test.Name)));
+        }
+
+        private static XElement TestEntry(TestResult test, Guid testListId)
+        {
+            var ns = XNamespace.Get("http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+
+            return new XElement(ns + "TestEntry",
+                new XAttribute("testId", test.Id),
+                new XAttribute("executionId", test.ExecutionId),
+                new XAttribute("testListId", testListId));
+        }
+
+        private static XElement TestResult(TestResult test, Guid testListId)
+        {
+            var ns = XNamespace.Get("http://microsoft.com/schemas/VisualStudio/TeamTest/2010");
+
+            XElement output = null;
+
+            // Assume output is error on failure and trace on success
+            if (!test.Success)
+            {
+                output = new XElement(ns + "Output",
+                    new XElement(ns + "ErrorInfo",
+                        new XElement(ns + "Message",
+                            test.Output)));
+            }
+            else if (!string.IsNullOrEmpty(test.Output))
+            {
+                output = new XElement(ns + "Output",
+                    new XElement(ns + "DebugTrace",
+                        test.Output));
+            }
+
+            return new XElement(ns + "UnitTestResult",
+                new XAttribute("executionId", test.ExecutionId),
+                new XAttribute("testId", test.Id),
+                new XAttribute("testName", test.Name),
+                new XAttribute("outcome", test.Success ? "Passed" : "Failed"),
+                new XAttribute("testListId", testListId),
+                new XAttribute("duration", test.Duration.ToString()),
+                output);
+        }
+    }
+}
